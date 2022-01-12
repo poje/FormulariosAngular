02505766dba968f5089753cf5ca62e9381017d92
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2022-01-12 11:49:26 -0300

    Se trabajan ejemplos con formularios reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent implements OnInit {
-
-  constructor() { }
+  constructor(private fb: FormBuilder)  {}
 
   ngOnInit(): void {
+      this.miFormulario.reset({
+        nombre:'RTX 4080ti',
+        precio: 1600
+      })
   }
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre': new FormControl('RTX 4080 TI'),
+  //   'precio': new FormControl('0'),
+  //   'existencias': new FormControl('5')
+  // });
 
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]], //Valor, Validacion Sincrona, Validación Asincrona
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  })
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
   ngOnInit(): void {
+    this.miFormulario.reset({...this.persona, condiciones: false });
+
+    this.miFormulario.valueChanges.subscribe(({ condiciones, ...rest}) => {
+      this.persona = rest;
+    })
   }
 
+  miFormulario:FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [true, Validators.required ],
+    condiciones: [false, Validators.requiredTrue ]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  guardar() {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
+    console.log(formValue);
+  }
 }
